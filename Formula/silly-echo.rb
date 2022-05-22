83305a9bd4b7dfd4268af90435b1dd0ae6b56893
axylos <robertdraketalley@gmail.com>
2022-05-22 14:21:03 -0400

    change to alpha

--- Formula/silly-echo.rb
+++ Formula/silly-echo.rb
@@ -1,17 +1,24 @@
 class SillyEcho < Formula
   desc "It does a thing"
   homepage "https://draketalley.com"
-  url "https://github.com/Axylos/silly/archive/refs/tags/v0.1.1-rc.8.tar.gz"
-  version "0.1.1.-beta"
+  version "0.1.1.alpha.1"
   sha256 "f38dafb6b0552b9c9785f418aa7138953a4d9d536a079db86ea294f74957bb79"
   license "MIT"
 
   depends_on "rust" => :build
+  #if OS.mac?
+  #  url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-apple-darwin.tar.gz"
+  #  sha256 "585c18350cb8d4392461edd6c921e6edd5a97cbfc03b567d7bd440423e118082"
+  #elsif OS.linux?
+  url "https://github.com/axylos/silly/releases/download/#{version}/silly-echo-#{version}-x86_64-unknown-linux-musl.tar.gz"
 
   bottle do
-    root_url "https://github.com/Axylos/silly/releases/download/v0.1.1-rc.8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7543fc01bd554435fb65d1eff71963e41d0bd08d3c197b4806ccdef980d8a812"
+    root_url "https://github.com/Axylos/silly/archive/refs/tags/#{version}/"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f22b1840e49965bd975fcaa0a26686a5982b81ec2eb0423ef5922cda8293685e"
   end
+
+  sha256 "ee4e0751ab108b6da4f47c52da187d5177dc371f0f512a7caaec5434e711c091"
+  #end
 
   def install
     system "cargo", "install", "--path", "silly-echo", "--root", prefix
