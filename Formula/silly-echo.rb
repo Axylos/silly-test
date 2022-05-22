46025fc3566c415a07c18bd926d15e414cfa2f62
axylos <robertdraketalley@gmail.com>
2022-05-22 17:13:42 -0400

    fix sha

--- Formula/silly-echo.rb
+++ Formula/silly-echo.rb
@@ -2,7 +2,7 @@ class SillyEcho < Formula
   desc "It does a thing"
   homepage "https://draketalley.com"
   version "v0.1.1.alpha.1"
-  sha256 "f38dafb6b0552b9c9785f418aa7138953a4d9d536a079db86ea294f74957bb79"
+  sha256 "b527d14b0d8caafa219a7eb9e62227f0322385d16c9b4e51c1e36227a00004a0"
   license "MIT"
 
   depends_on "rust" => :build
@@ -17,7 +17,6 @@ class SillyEcho < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "f22b1840e49965bd975fcaa0a26686a5982b81ec2eb0423ef5922cda8293685e"
   end
 
-  sha256 "ee4e0751ab108b6da4f47c52da187d5177dc371f0f512a7caaec5434e711c091"
   #end
 
   def install
